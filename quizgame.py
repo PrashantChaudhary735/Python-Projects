821c054a70a06bf7f6fc558fadb8b299b6724953
Prashant Chaudhary <chaudharyprashant7355@gmail.com>
2025-06-28 15:57:41 +0530

    Quiz Game

--- quizgame.py
+++ quizgame.py
@@ -6,39 +6,47 @@ if playing.lower() != "yes":
     quit()
 
 print("Okay! Let's play :) ")
+score = 0
 
 # 1st Question:
 answer = input("What does CPU stands for? ")
-
 if answer.lower() == "central processing unit":
     print("Correct!")
+    score = score + 1
 else:
     print("Incorrect!")
 
 # 2nd Question:
 
 answer = input("What does GPU stands for? ")
-
 if answer.lower() == "graphical processing unit":
     print("Correct!")
+    score = score + 1
 else:
     print("Incorrect!")
 
 
 answer = input("What does RAM stands for? ")
-
 if answer.lower() == "random access memory":
     print("Correct!")
+    score = score + 1
 else:
     print("Incorrect!")
 
 
 answer = input("What does PSU stands for? ")
-
 if answer.lower() == "power supply unit":
     print("Correct!")
+    score = score + 1
 else:
     print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/4) * 100) + "% of questions correct!")
+
+
+
+
 
 
 
